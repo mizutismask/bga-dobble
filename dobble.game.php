b9fa06d67f0d9ab208f65f3e9b5345e2403b9c98
mizutismask <severinek@gmail.com>
2021-04-11 10:43:26 +0200

    add options game state labels

--- dobble.game.php
+++ dobble.game.php
@@ -1,23 +1,25 @@
 <?php
- /**
-  *------
-  * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
-  * Dobble implementation : © <Your name here> <Your email address here>
-  * 
-  * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
-  * See http://en.boardgamearena.com/#!doc/Studio for more information.
-  * -----
-  * 
-  * dobble.game.php
-  *
-  * This is the main file for your game logic.
-  *
-  * In this PHP file, you are going to defines the rules of the game.
-  *
-  */
+
+/**
+ *------
+ * BGA framework: © Gregory Isabelli <gisabelli@boardgamearena.com> & Emmanuel Colin <ecolin@boardgamearena.com>
+ * Dobble implementation : © <Your name here> <Your email address here>
+ * 
+ * This code has been produced on the BGA studio platform for use on http://boardgamearena.com.
+ * See http://en.boardgamearena.com/#!doc/Studio for more information.
+ * -----
+ * 
+ * dobble.game.php
+ *
+ * This is the main file for your game logic.
+ *
+ * In this PHP file, you are going to defines the rules of the game.
+ *
+ */
 
 
-require_once( APP_GAMEMODULE_PATH.'module/table/table.game.php' );
+require_once(APP_GAMEMODULE_PATH . 'module/table/table.game.php');
+require_once("modules/php/constants.inc.php");
 
 if (!defined('DECK_LOC_DECK')) {
 
@@ -39,8 +41,8 @@ if (!defined('DECK_LOC_DECK')) {
 
 class Dobble extends Table
 {
-	function __construct( )
-	{
+    function __construct()
+    {
         // Your global variables labels:
         //  Here, you can assign labels to global variables you are using for this game.
         //  You can use any number of global variables with IDs between 10 and 99.
@@ -48,25 +50,31 @@ class Dobble extends Table
         //  the corresponding ID in gameoptions.inc.php.
         // Note: afterwards, you can get/set the global variables with getGameStateValue/setGameStateInitialValue/setGameStateValue
         parent::__construct();
-        
-        self::initGameStateLabels( array( 
+
+        self::initGameStateLabels(array(
             //    "my_first_global_variable" => 10,
             //    "my_second_global_variable" => 11,
             //      ...
             //    "my_first_game_variant" => 100,
             //    "my_second_game_variant" => 101,
             //      ...
-        ) );   
-        
-          $this->deck = self::getNew("module.common.deck");
+            "type_of_rules" => TYPE_OF_RULES,
+            "towering_inferno" => TOWERING_INFERNO,
+            "well" => WELL,
+            "hot_potato" => HOT_POTATO,
+            "poisoned_gift" => POISONED_GIFT,
+            "triplet" => TRIPLET,
+        ));
+
+        $this->deck = self::getNew("module.common.deck");
         $this->deck->init("deck");
-	}
-	
-    protected function getGameName( )
+    }
+
+    protected function getGameName()
     {
-		// Used for translations and stuff. Please do not modify.
+        // Used for translations and stuff. Please do not modify.
         return "dobble";
-    }	
+    }
 
     /*
         setupNewGame:
@@ -75,28 +83,28 @@ class Dobble extends Table
         In this method, you must setup the game according to the game rules, so that
         the game is ready to be played.
     */
-    protected function setupNewGame( $players, $options = array() )
-    {    
+    protected function setupNewGame($players, $options = array())
+    {
         $this->setColorsBasedOnPreferences($players);
-        
+
         /************ Start the game initialization *****/
 
         // Init global values with their initial values
         //self::setGameStateInitialValue( 'my_first_global_variable', 0 );
-        
+
         // Init game statistics
         // (note: statistics used in this file must be defined in your stats.inc.php file)
         //self::initStat( 'table', 'table_teststat1', 0 );    // Init a table statistics
         //self::initStat( 'player', 'player_teststat1', 0 );  // Init a player statistics (for all players)
 
-         // setup the deck and deal card
-         $cards = array();
-         foreach ($this->cards_description as $name => $card) {
-             $cards[] = array('type' => $name, 'type_arg' => $card["type"], 'nbr' => 1);
-         }
-         $this->deck->createCards($cards, DECK_LOC_DECK);
-         $this->deck->shuffle(DECK_LOC_DECK);
-         $this->dealCards();
+        // setup the deck and deal card
+        $cards = array();
+        foreach ($this->cards_description as $name => $card) {
+            $cards[] = array('type' => $name, 'type_arg' => $card["type"], 'nbr' => 1);
+        }
+        $this->deck->createCards($cards, DECK_LOC_DECK);
+        $this->deck->shuffle(DECK_LOC_DECK);
+        $this->dealCards();
 
         // Activate first player (which is in general a good idea :) )
         $this->activeNextPlayer();
@@ -116,16 +124,16 @@ class Dobble extends Table
     protected function getAllDatas()
     {
         $result = array();
-    
+
         $current_player_id = self::getCurrentPlayerId();    // !! We must only return informations visible by this player !!
-    
+
         // Get information about players
         // Note: you can retrieve some extra field you added for "player" table in "dbmodel.sql" if you need it.
         $sql = "SELECT player_id id, player_score score FROM player ";
-        $result['players'] = self::getCollectionFromDb( $sql );
-  
+        $result['players'] = self::getCollectionFromDb($sql);
+
         // TODO: Gather all information about current game situation (visible by player $current_player_id).
-  
+
         return $result;
     }
 
@@ -147,9 +155,9 @@ class Dobble extends Table
     }
 
 
-//////////////////////////////////////////////////////////////////////////////
-//////////// Utility functions
-////////////    
+    //////////////////////////////////////////////////////////////////////////////
+    //////////// Utility functions
+    ////////////    
 
     function setColorsBasedOnPreferences($players)
     {
@@ -176,22 +184,32 @@ class Dobble extends Table
     function dealCards()
     {
         $players = self::loadPlayersBasicInfos();
-        $number = 1;
-        foreach ($players as $player_id => $player) {
-            $this->pickCardsAndNotifyPlayer($number, $player_id);
+
+        if ($this->getMiniGame() === TRIPLET) {
+        } else if ($this->getMiniGame() === WELL) {
+        } else {
+            $number = 1;
+            foreach ($players as $player_id => $player) {
+                $this->pickCardsAndNotifyPlayer($number, $player_id);
+            }
         }
     }
 
     function pickCardsAndNotifyPlayer($nb, $player_id)
-    { 
+    {
         $cards = $this->deck->pickCards($nb, DECK_LOC_DECK, $player_id);
         // Notify player about his cards
         self::notifyPlayer($player_id, NOTIF_HAND_CHANGE, '', array('added' => $cards));
     }
 
-//////////////////////////////////////////////////////////////////////////////
-//////////// Player actions
-//////////// 
+    function getMiniGame()
+    {
+        return self::getGameStateValue('type_of_rules');
+    }
+
+    //////////////////////////////////////////////////////////////////////////////
+    //////////// Player actions
+    //////////// 
 
     /*
         Each time a player is doing some game action, one of the methods below is called.
@@ -224,10 +242,10 @@ class Dobble extends Table
     
     */
 
-    
-//////////////////////////////////////////////////////////////////////////////
-//////////// Game state arguments
-////////////
+
+    //////////////////////////////////////////////////////////////////////////////
+    //////////// Game state arguments
+    ////////////
 
     /*
         Here, you can create methods defined as "game state arguments" (see "args" property in states.inc.php).
@@ -252,15 +270,15 @@ class Dobble extends Table
     }    
     */
 
-//////////////////////////////////////////////////////////////////////////////
-//////////// Game state actions
-////////////
+    //////////////////////////////////////////////////////////////////////////////
+    //////////// Game state actions
+    ////////////
 
     /*
         Here, you can create methods defined as "game state actions" (see "action" property in states.inc.php).
         The action method of state X is called everytime the current game state is set to X.
     */
-    
+
     /*
     
     Example for game state "MyGameState":
@@ -274,9 +292,9 @@ class Dobble extends Table
     }    
     */
 
-//////////////////////////////////////////////////////////////////////////////
-//////////// Zombie
-////////////
+    //////////////////////////////////////////////////////////////////////////////
+    //////////// Zombie
+    ////////////
 
     /*
         zombieTurn:
@@ -291,15 +309,15 @@ class Dobble extends Table
         you must _never_ use getCurrentPlayerId() or getCurrentPlayerName(), otherwise it will fail with a "Not logged" error message. 
     */
 
-    function zombieTurn( $state, $active_player )
+    function zombieTurn($state, $active_player)
     {
-    	$statename = $state['name'];
-    	
+        $statename = $state['name'];
+
         if ($state['type'] === "activeplayer") {
             switch ($statename) {
                 default:
-                    $this->gamestate->nextState( "zombiePass" );
-                	break;
+                    $this->gamestate->nextState("zombiePass");
+                    break;
             }
 
             return;
@@ -307,17 +325,17 @@ class Dobble extends Table
 
         if ($state['type'] === "multipleactiveplayer") {
             // Make sure player is in a non blocking status for role turn
-            $this->gamestate->setPlayerNonMultiactive( $active_player, '' );
-            
+            $this->gamestate->setPlayerNonMultiactive($active_player, '');
+
             return;
         }
 
-        throw new feException( "Zombie mode not supported at this game state: ".$statename );
+        throw new feException("Zombie mode not supported at this game state: " . $statename);
     }
-    
-///////////////////////////////////////////////////////////////////////////////////:
-////////// DB upgrade
-//////////
+
+    ///////////////////////////////////////////////////////////////////////////////////:
+    ////////// DB upgrade
+    //////////
 
     /*
         upgradeTableDb:
@@ -329,32 +347,32 @@ class Dobble extends Table
         update the game database and allow the game to continue to run with your new version.
     
     */
-    
-    function upgradeTableDb( $from_version )
+
+    function upgradeTableDb($from_version)
     {
         // $from_version is the current version of this game database, in numerical form.
         // For example, if the game was running with a release of your game named "140430-1345",
         // $from_version is equal to 1404301345
-        
+
         // Example:
-//        if( $from_version <= 1404301345 )
-//        {
-//            // ! important ! Use DBPREFIX_<table_name> for all tables
-//
-//            $sql = "ALTER TABLE DBPREFIX_xxxxxxx ....";
-//            self::applyDbUpgradeToAllDB( $sql );
-//        }
-//        if( $from_version <= 1405061421 )
-//        {
-//            // ! important ! Use DBPREFIX_<table_name> for all tables
-//
-//            $sql = "CREATE TABLE DBPREFIX_xxxxxxx ....";
-//            self::applyDbUpgradeToAllDB( $sql );
-//        }
-//        // Please add your future database scheme changes here
-//
-//
+        //        if( $from_version <= 1404301345 )
+        //        {
+        //            // ! important ! Use DBPREFIX_<table_name> for all tables
+        //
+        //            $sql = "ALTER TABLE DBPREFIX_xxxxxxx ....";
+        //            self::applyDbUpgradeToAllDB( $sql );
+        //        }
+        //        if( $from_version <= 1405061421 )
+        //        {
+        //            // ! important ! Use DBPREFIX_<table_name> for all tables
+        //
+        //            $sql = "CREATE TABLE DBPREFIX_xxxxxxx ....";
+        //            self::applyDbUpgradeToAllDB( $sql );
+        //        }
+        //        // Please add your future database scheme changes here
+        //
+        //
 
 
-    }    
+    }
 }
